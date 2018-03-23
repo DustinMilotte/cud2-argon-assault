dc10fda2931ceb08c440403d944c767ce39512c4
Dustin Milotte <dmilotte@gmail.com>
2018-03-23 14:20:04 -0400

    add skybox and music player

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+    public AudioSource audioSource;
+    public static MusicPlayer instance;
+
+    private void Awake() {
+        if(instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadFirstLevel", 3f);
+	}
+
+    void LoadFirstLevel() { 
+        SceneManager.LoadScene(1);
+    }
+}
