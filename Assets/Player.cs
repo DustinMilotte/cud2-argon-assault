71fe4e284221e4840f9185c30d90624bd4f29aea
Dustin Milotte <dmilotte@gmail.com>
2018-03-24 14:20:20 -0400

    add waypoint circuit, start player movement

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xSpeed * xThrow * Time.deltaTime;
+        print(xOffset);
+	}
+}
