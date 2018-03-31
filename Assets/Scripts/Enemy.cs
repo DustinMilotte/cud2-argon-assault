c17188d64615d96de71c51359833b192cd5e33a9
Dustin Milotte <dmilotte@gmail.com>
2018-03-31 14:33:57 -0400

    add enemy death fx, start ui

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+    private void Start(){
+        BoxCollider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other){
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        print(other);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
