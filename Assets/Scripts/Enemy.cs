b39c5011db360551654deef8e9f12068c8290c08
Dustin Milotte <dmilotte@gmail.com>
2018-04-01 19:06:44 -0400

    add score keeping methods and enemy point value

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,26 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    Boolean isDead = false;
+
+    [SerializeField] int scorePerHit = 10;
+
+    ScoreBoard scoreBoard;
 
     private void Start(){
         BoxCollider collider = gameObject.AddComponent<BoxCollider>();
         collider.isTrigger = false;
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void OnParticleCollision(GameObject other){
-        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
-        print(other);
-        fx.transform.parent = parent;
-        Destroy(gameObject);
+        if (!isDead){
+            isDead = true;
+            GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+            fx.transform.parent = parent;
+            scoreBoard.Scorehit(scorePerHit);
+            Destroy(gameObject);
+        }
+       
     }
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+    int score = 0;
+    Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+	}
+	
+	public void Scorehit(int scoreIncrease){
+        score += scoreIncrease;
+        scoreText.text = score.ToString();
+    }
+}
